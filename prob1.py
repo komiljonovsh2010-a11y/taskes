d674640f1c5d40bc0cdd9557e145451b0c66a8a8
kshohrux1805 <komiljonovsh2010@gmail.com>
2026-04-26 17:28:43 +0500

    Function is ready

--- prob1.py
+++ prob1.py
@@ -1,2 +1,6 @@
-def unliharflar(word:str):
+def unliharflar(words:str):
     unlilar=["aeuioAEUIO"]
+    count=sum(word for word in words if word.isalpha() if word in unlilar)
+    c1=sum(word for word in words if word.isalpha() if word not in unlilar)
+    let={"Unlilar": count,"Undoshlar":c1}
+    print(let)
