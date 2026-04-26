1e3a71c86b5c801218f24f8fc4e5c02f84683904
kshohrux1805 <komiljonovsh2010@gmail.com>
2026-04-26 17:43:59 +0500

    first work is already

---
+++ prob2.py
@@ -0,0 +1,7 @@
+find_top_seller=(
+    {"Olma": 5000, "Banan": 8000, "Uzum": 7000},
+    {"Olma": 10,   "Banan": 5,    "Uzum": 8}
+)
+for product in find_top_seller:
+    for name in product:
+        print(name)
