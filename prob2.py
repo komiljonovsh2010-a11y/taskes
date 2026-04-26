b8171271502b7e7f382258d2a6fcdd51c3350caf
kshohrux1805 <komiljonovsh2010@gmail.com>
2026-04-26 18:23:37 +0500

    already

--- prob2.py
+++ prob2.py
@@ -2,6 +2,12 @@ find_top_seller=(
     {"Olma": 5000, "Banan": 8000, "Uzum": 7000},
     {"Olma": 10,   "Banan": 5,    "Uzum": 8}
 )
-for product in find_top_seller:
-    for name in product:
-        print(name)
+
+qyt=find_top_seller[1]
+products=find_top_seller[0]
+shop=tuple((name,product*price)
+             for i,(name,product) in enumerate(products.items())
+             for n,price in enumerate(qyt.values())
+             if i==n)
+max_product=max(shop,key=lambda x : x[-1])
+print(f"Eng daromadli mahsulot\n{max_product[0]} : {max_product[-1]}")
