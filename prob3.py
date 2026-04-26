7dc373fbb85d44755757c208c7629b1fa219e00b
kshohrux1805 <komiljonovsh2010@gmail.com>
2026-04-26 19:49:16 +0500

    Funksiya tayyor

--- prob3.py
+++ prob3.py
@@ -1,7 +1,6 @@
 rearrange_by_frequency=[4, 5, 6, 5, 4, 3, 4]
-numbers={}
-for index,number in enumerate(rearrange_by_frequency):
-    if number==rearrange_by_frequency[index+1]:
-        c+=1
-        numbers={number: c}
-print(numbers)
+from collections import Counter
+def frequency_sort(nums: list[int]) -> list[int]:
+    freq = Counter(nums)
+    return sorted(nums, key=lambda x: (-freq[x], x))
+print(frequency_sort(rearrange_by_frequency))
